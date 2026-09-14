06f2946802030bc97f7f5334c0d8c775c8718aae
nob <no.nob.n5.biz@gmail.com>
2026-09-14 16:26:43 +0900

    refactor: Remove unused User model import from QaReplyController and AnnouncementFactory

--- database/factories/AnnouncementFactory.php
+++ database/factories/AnnouncementFactory.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Database\Factories;
 
 use App\Enums\AnnouncementTargetType;
-use App\Enums\UserRole;
 use App\Models\Announcement;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
